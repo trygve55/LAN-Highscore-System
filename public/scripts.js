ac73db2efa65aa0262ea7d8b1c2cae2a65302318
Trygve Nerland <trygve55@gmail.com>
2018-10-12 14:03:40 +0200

    All normal features working

--- public/scripts.js
+++ public/scripts.js
@@ -10,13 +10,13 @@ function addHighscore(highscore, playerName) {
         },
         body: JSON.stringify({score : highscore, playerName : playerName})
     }).then(
-        response => console.log(response)
+        response => location.reload()
     ).catch(
         error => console.log(error) // Handle the error response object
     );
 }
 
-function addGame(gameName, playerName) {
+function addGame(gameName) {
     fetch('/api/games?token=' + localStorage.token, {
         method: 'POST',
         headers:{
@@ -24,7 +24,7 @@ function addGame(gameName, playerName) {
         },
         body: JSON.stringify({game_name : gameName})
     }).then(
-        response => console.log(response)
+        response => location.reload()
     ).catch(
         error => console.log(error) // Handle the error response object
     );
@@ -52,7 +52,7 @@ function removeHighscore(highscoreId) {
         },
         body: JSON.stringify({})
     }).then(
-        response => console.log(response)
+        response => location.reload()
     ).catch(
         error => console.log(error) // Handle the error response object
     );
@@ -73,14 +73,14 @@ function removeAllGames() {
 }
 
 function removeGame(gameId) {
-    fetch('/api/games/' + getGameId() + '/reset?token=' + localStorage.token, {
+    fetch('/api/games/' + gameId + '?token=' + localStorage.token, {
         method: 'DELETE',
         headers:{
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({})
     }).then(
-        response => console.log(response)
+        response => location.reload()
     ).catch(
         error => console.log(error) // Handle the error response object
     );
@@ -98,6 +98,8 @@ function login(username, password) {
         response => response.json() // if the response is a JSON object
     ).then(
         success => localStorage.token = success.token
+    ).then(
+        test => location.reload()
     ).catch(
         error => console.log(error) // Handle the error response object
     );
@@ -108,10 +110,75 @@ function addNewHighscore() {
     location.reload();
 }
 
+function removeOldHighscore(highscoreId) {
+    removeHighscore(highscoreId);
+}
+
+function addNewGame() {
+    addGame(document.getElementById("newGame").value);
+    location.reload();
+}
+
+function removeOldGame(event, gameId) {
+    event.stopPropagation();
+    removeGame(gameId);
+}
+
+function showLoginLogout() {
+    if (localStorage.token)
+        return logout();
+
+    loginForm = document.getElementById('loginForm');
+    if (loginForm.style.display === 'none') {
+        loginForm.style.display = 'inline';
+        document.getElementById('username').focus();
+
+        var inputsLogin = document.getElementsByClassName('loginInput');
+
+        for(var i = 0; i < inputsLogin.length; i++) {
+            console.log("test3");
+            inputsLogin[i].addEventListener("keyup", function(event) {
+                console.log('test');
+//                    event.preventDefault();
+                if (event.keyCode === 13) {
+                    document.getElementById("loginButton").click();
+                }
+            });
+        }
+
+    } else
+        loginForm.style.display = 'none'
+}
+
+function sendLogin() {
+    login(document.getElementById('username').value, document.getElementById('password').value);
+}
+
+function logout() {
+    localStorage.removeItem('token');
+    location.reload();
+}
+
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
         if (localStorage.token) {
-            document.getElementById("addNew").classList.remove("hidden");
+            var elm = document.getElementsByClassName("hiddenAdmin");
+            for (let i = 0; i < elm.length;) {
+                elm[0].classList.remove("hiddenAdmin")
+            }
+
+            var inputs = document.getElementsByClassName('tableInput');
+
+            for(var i = 0; i < inputs.length; i++) {
+                inputs[i].addEventListener("keyup", function(event) {
+                    event.preventDefault();
+                    if (event.keyCode === 13) {
+                        document.getElementById("submitButton").click();
+                    }
+                });
+            }
+
+            document.getElementById('showLoginButton').innerText = 'Logout';
         }
     }
 };
